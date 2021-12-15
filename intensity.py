e9e72c16c3d932e5551497d970d07bcecfaf8071
Mojtaba <m.nosratlo@gmail.com>
2021-12-15 13:10:02 +0100

    test

--- intensity.py
+++ intensity.py
@@ -76,6 +76,8 @@ for image_index in frames_with_cells:
     sum_frames = sum_frames +im_np
 print(num_frames)
 
+
+#fd
 flatfield_correction = sum_frames/ num_frames
 flatfield_correction = flatfield_correction/np.mean(flatfield_correction)
 
